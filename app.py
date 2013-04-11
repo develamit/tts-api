8638eab68c56d4fcc5b39eb0949b87a907f6254f
Gavin Hackeling <gavinhackeling@gmail.com>
2013-04-11 12:24:19 -0400

    retrieval and joining complete

--- app.py
+++ app.py
@@ -4,76 +4,73 @@
 
 '''
 import urllib2
-import urllib
+from urllib import quote_plus
 import shutil
-import os
+from os import urandom
+import subprocess
+from os.path import dirname, realpath, join
 import tornado.web
 import tornado.ioloop
 from tornado.options import define, options
-
-
-class CheckHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write('service minimally online')
-
+from text_segmenter import get_segments
+from binascii import hexlify
 
 class TTSHandler(tornado.web.RequestHandler):
     def get(self):
-        text = self.get_argument("text", strip=True)
-        base_url = 'http://translate.google.com/translate_tts?tl=en&q='
-
-        # segment text
-        part1 = urllib.quote_plus('John Winston Ono Lennon, MBE  was an English musician and singer-songwriter who rose to worldwide')
-        part2 = urllib.quote_plus('fame as one of the founder members of The Beatles, one of the most commercially successful and ')
-        part3 = urllib.quote_plus('critically acclaimed acts in the history of popular music.')
+        joined_name = hexlify(urandom(16))
 
         # Set the user agent
         opener = urllib2.build_opener()
         opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        base_url = 'http://translate.google.com/translate_tts?tl=en&q='
 
-        files = ['mp3/part1.mp3', 'mp3/part2.mp3', 'mp3/part3.mp3']
+        # Split the text into <100 character segments
+        text = self.get_argument("text", strip=True)
+        segments = get_segments(text)
+        segments = [quote_plus(s) for s in segments]
 
-        # Get the parts
-        response = opener.open(base_url + part1)
-        localFile = open('mp3/part1.mp3', 'w')
-        localFile.write(response.read())
-        localFile.close()
+        trimmed_names = []
+        for segment in segments:
+            name = hexlify(urandom(16))
+            #try:
+            tts_response = opener.open(base_url + segment)
+            raw_name = 'static/mp3/%s_r.mp3' % name
+            trimmed_name = 'static/mp3/%s_t.mp3' % name
 
-        response = opener.open(base_url + part2)
-        localFile = open('mp3/part2.mp3', 'w')
-        localFile.write(response.read())
-        localFile.close()
+            raw_file = open(raw_name, 'w')
+            raw_file.write(tts_response.read())
+            raw_file.close()
 
-        response = opener.open(base_url + part3)
-        localFile = open('mp3/part3.mp3', 'w')
-        localFile.write(response.read())
-        localFile.close()
+            sox_call = ['sox',
+                dirname(realpath(__file__)) + '/' + raw_name,
+                dirname(realpath(__file__)) + '/'+ trimmed_name,
+                'silence', '1', '0', '-40d']
+            return_code = subprocess.call(sox_call)
+            if return_code != 0:
+                print 'error'
+
+            trimmed_names.append(trimmed_name)
+
 
         # Cat the parts
-        PATH = r'/home/gavin/dev/tts-api/mp3'
+        PATH = dirname(realpath(__file__)) + '/static/mp3/joined/'
 
-        response_file_name = '/home/ubuntu/www/static/mp3/merged.mp3'
+        response_file_name = PATH + joined_name + '.mp3'
         with open(response_file_name, 'wb') as destination:
-            for filename in files:
-                with open(filename, 'rb') as in_:
+            for file_name in trimmed_names:
+                with open(file_name, 'rb') as in_:
                     shutil.copyfileobj(in_, destination)
 
-        response = 'http://localhost/static/mp3/merged.mp3'
-
-        self.redirect(response)
-        #with open(response_file_name, "r") as response_file:
-            #self.write(response_file.read())
-            # delete file
+        self.redirect(response_file_name)
 
 
 handlers = [
-            (r"/check", CheckHandler),
             (r"/tts", TTSHandler),
             ]
 
 
-settings = dict(template_path=os.path.join(
-    os.path.dirname(__file__), "templates"))
+settings = dict(template_path=join(
+    dirname(__file__), "templates"))
 application = tornado.web.Application(handlers, **settings)
 define("port", default=8007, help="run on the given port", type=int)
 
---
+++ text_segmenter.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+def get_segments(text):
+    segments = [
+    'John Winston Ono Lennon, MBE  was an English musician and singer-songwriter who rose to worldwide',
+    'fame as one of the founder members of The Beatles, one of the most commercially successful and ',
+    'critically acclaimed acts in the history of popular music.'
+        ]
+    return segments
